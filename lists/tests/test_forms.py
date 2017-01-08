b22a51bd30140d396859ba6619f633ccb304bc30
Michael Collins <michaelj.collinsuk@gmail.com>
2017-01-08 23:05:04 +0000

    Add save function to ItemForm and refactored views to utilise it. All tests passing

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -3,6 +3,7 @@ EMPTY_ITEM_ERROR = "You can't have an empty list item"
 from django.test import TestCase
 
 from lists.forms import ItemForm
+from lists.models import List, Item
 
 class ItemFormTest(TestCase):
 
@@ -15,3 +16,11 @@ class ItemFormTest(TestCase):
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
+
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
