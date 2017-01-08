7e3c16c7538f76d56edb64b04f18331d56efe78a
Michael Collins <michaelj.collinsuk@gmail.com>
2017-01-08 10:41:48 +0000

    Started FT for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -104,3 +104,25 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+
+    def test_layout_and_styling(self):
+        # User goes to homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # User sees centred input box
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
+        # user starts list and box is centererd there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
